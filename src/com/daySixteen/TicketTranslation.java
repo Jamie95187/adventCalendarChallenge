13a07d7a1c82b0de9d9646653f314da5f61b0ef3
Jamie <jamiewong63@googlemail.com>
2021-01-28 05:17:40 +0000

    day sixteen ticket translation worked out error count for example data

--- src/com/daySixteen/TicketTranslation.java
+++ src/com/daySixteen/TicketTranslation.java
@@ -15,18 +15,24 @@ public class TicketTranslation {
 
     List<int[]> rules = new ArrayList<>();
     List<String> nearbyTickets = new ArrayList<>();
+    List<Integer> errors = new ArrayList<>();
 
     private void getRules() {
         try {
-            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Rules.txt";
+//            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Rules.txt";
+            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleRules.txt";
             String content = Files.readString(Paths.get(path), StandardCharsets.US_ASCII);
             Pattern p = Pattern.compile("\\d+");
             Matcher m = p.matcher(content);
             while (m.find()) {
-                int[] rule = new int[2];
+                int[] rule = new int[4];
                 rule[0] = Integer.parseInt(m.group());
                 m.find();
                 rule[1] = Integer.parseInt(m.group());
+                m.find();
+                rule[2] = Integer.parseInt(m.group());
+                m.find();
+                rule[3] = Integer.parseInt(m.group());
                 rules.add(rule);
             }
         } catch (IOException e) {
@@ -38,7 +44,8 @@ public class TicketTranslation {
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader(
-                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Tickets.txt"
+//                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Tickets.txt"
+                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleData.txt"
             ));
             String line = reader.readLine();
             while (line != null) {
@@ -51,16 +58,36 @@ public class TicketTranslation {
         }
     }
 
-    private int countErrors() {
+    private void getErrors() {
+        int errorCount = 0;
         for (int i = 0; i < nearbyTickets.size(); i++) {
             String[] values = nearbyTickets.get(i).split(",");
-            for ()
+            for (int j = 0; j < values.length; j++) {
+                int value = Integer.parseInt(values[j]);
+                boolean validValue = false;
+                for (int k = 0; k < rules.size(); k++) {
+//                    System.out.println("VALUE " + value);
+//                    System.out.println("FIRST LOWER BOUND : " + rules.get(k)[0] +  " FIRST UPPER BOUND : " + rules.get(k)[1]);
+//                    System.out.println("SECOND LOWER BOUND : " + rules.get(k)[2] +  " SECOND UPPER BOUND : " + rules.get(k)[3]);
+                    if (rules.get(k)[0] <= value && rules.get(k)[1] >= value || rules.get(k)[2] <= value && rules.get(k)[3] >= value) {
+                        validValue = true;
+                        break;
+                    }
+                }
+                if (!validValue) {
+                    errors.add(value);
+                    errorCount += value;
+                }
+            }
         }
+        System.out.println("error Count : " + errorCount);
     }
 
     public void test() {
         getRules();
         getNearbyTickets();
+        getErrors();
+        System.out.println(errors.size());
     }
 
 }
