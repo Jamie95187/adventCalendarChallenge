9ba42ef76e806474a57eb81ae5b3021a9f8e925e
Jamie <jamiewong63@googlemail.com>
2021-02-02 06:21:40 +0000

    day sixteen values do not exclusively fit into one rule.

--- src/com/daySixteen/TicketTranslation.java
+++ src/com/daySixteen/TicketTranslation.java
@@ -20,8 +20,8 @@ public class TicketTranslation {
 
     private void getRules() {
         try {
-            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Rules.txt";
-//            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleRulesPartTwo.txt";
+//            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Rules.txt";
+            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleRulesPartTwo.txt";
 //            String path = "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleRules.txt";
             String content = Files.readString(Paths.get(path), StandardCharsets.US_ASCII);
             Pattern p = Pattern.compile("\\d+");
@@ -46,8 +46,8 @@ public class TicketTranslation {
         BufferedReader reader;
         try {
             reader = new BufferedReader(new FileReader(
-                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Tickets.txt"
-//                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleDataPartTwo.txt"
+//                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/Tickets.txt"
+                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleDataPartTwo.txt"
 //                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySixteen/exampleData.txt"
             ));
             String line = reader.readLine();
@@ -70,9 +70,6 @@ public class TicketTranslation {
                 int value = Integer.parseInt(values[j]);
                 boolean validValue = false;
                 for (int k = 0; k < rules.size(); k++) {
-//                    System.out.println("VALUE " + value);
-//                    System.out.println("FIRST LOWER BOUND : " + rules.get(k)[0] +  " FIRST UPPER BOUND : " + rules.get(k)[1]);
-//                    System.out.println("SECOND LOWER BOUND : " + rules.get(k)[2] +  " SECOND UPPER BOUND : " + rules.get(k)[3]);
                     if (rules.get(k)[0] <= value && rules.get(k)[1] >= value || rules.get(k)[2] <= value && rules.get(k)[3] >= value) {
                         validValue = true;
                         break;
@@ -103,24 +100,24 @@ public class TicketTranslation {
         List<String> zero_index = new ArrayList<String>();
         List<String> one_index = new ArrayList<String>();
         List<String> two_index = new ArrayList<String>();
-        List<String> three_index = new ArrayList<String>();
-        List<String> four_index = new ArrayList<String>();
-        List<String> five_index = new ArrayList<String>();
-        List<String> six_index = new ArrayList<String>();
-        List<String> seven_index = new ArrayList<String>();
-        List<String> eight_index = new ArrayList<String>();
-        List<String> nine_index = new ArrayList<String>();
-        List<String> ten_index = new ArrayList<String>();
-        List<String> eleven_index= new ArrayList<String>();
-        List<String> twelve_index = new ArrayList<String>();
-        List<String> thirteen_index = new ArrayList<String>();
-        List<String> fourteen_index = new ArrayList<String>();
-        List<String> fifteen_index = new ArrayList<String>();
-        List<String> sixteen_index = new ArrayList<String>();
-        List<String> seventeen_index = new ArrayList<String>();
-        List<String> eighteen_index = new ArrayList<String>();
-        List<String> nineteen_index = new ArrayList<String>();
-        List<String> twenty_index = new ArrayList<String>();
+//        List<String> three_index = new ArrayList<String>();
+//        List<String> four_index = new ArrayList<String>();
+//        List<String> five_index = new ArrayList<String>();
+//        List<String> six_index = new ArrayList<String>();
+//        List<String> seven_index = new ArrayList<String>();
+//        List<String> eight_index = new ArrayList<String>();
+//        List<String> nine_index = new ArrayList<String>();
+//        List<String> ten_index = new ArrayList<String>();
+//        List<String> eleven_index= new ArrayList<String>();
+//        List<String> twelve_index = new ArrayList<String>();
+//        List<String> thirteen_index = new ArrayList<String>();
+//        List<String> fourteen_index = new ArrayList<String>();
+//        List<String> fifteen_index = new ArrayList<String>();
+//        List<String> sixteen_index = new ArrayList<String>();
+//        List<String> seventeen_index = new ArrayList<String>();
+//        List<String> eighteen_index = new ArrayList<String>();
+//        List<String> nineteen_index = new ArrayList<String>();
+//        List<String> twenty_index = new ArrayList<String>();
         for (int i = 0; i < validNearbyTickets.size(); i++) {
             String[] values = validNearbyTickets.get(i).split(",");
             for(int j = 0; j < values.length; j++) {
@@ -134,64 +131,64 @@ public class TicketTranslation {
                     case 2:
                         two_index.add(values[j]);
                         break;
-                    case 3:
-                        three_index.add(values[j]);
-                        break;
-                    case 4:
-                        four_index.add(values[j]);
-                        break;
-                    case 5:
-                        five_index.add(values[j]);
-                        break;
-                    case 6:
-                        six_index.add(values[j]);
-                        break;
-                    case 7:
-                        seven_index.add(values[j]);
-                        break;
-                    case 8:
-                        eight_index.add(values[j]);
-                        break;
-                    case 9:
-                        nine_index.add(values[j]);
-                        break;
-                    case 10:
-                        ten_index.add(values[j]);
-                        break;
-                    case 11:
-                        eleven_index.add(values[j]);
-                        break;
-                    case 12:
-                        twelve_index.add(values[j]);
-                        break;
-                    case 13:
-                        thirteen_index.add(values[j]);
-                        break;
-                    case 14:
-                        fourteen_index.add(values[j]);
-                        break;
-                    case 15:
-                        fifteen_index.add(values[j]);
-                        break;
-                    case 16:
-                        sixteen_index.add(values[j]);
-                        break;
-                    case 17:
-                        seventeen_index.add(values[j]);
-                        break;
-                    case 18:
-                        eighteen_index.add(values[j]);
-                        break;
-                    case 19:
-                        nineteen_index.add(values[j]);
-                        break;
-                    case 20:
-                        twenty_index.add(values[j]);
-                        break;
+//                    case 3:
+//                        three_index.add(values[j]);
+//                        break;
+//                    case 4:
+//                        four_index.add(values[j]);
+//                        break;
+//                    case 5:
+//                        five_index.add(values[j]);
+//                        break;
+//                    case 6:
+//                        six_index.add(values[j]);
+//                        break;
+//                    case 7:
+//                        seven_index.add(values[j]);
+//                        break;
+//                    case 8:
+//                        eight_index.add(values[j]);
+//                        break;
+//                    case 9:
+//                        nine_index.add(values[j]);
+//                        break;
+//                    case 10:
+//                        ten_index.add(values[j]);
+//                        break;
+//                    case 11:
+//                        eleven_index.add(values[j]);
+//                        break;
+//                    case 12:
+//                        twelve_index.add(values[j]);
+//                        break;
+//                    case 13:
+//                        thirteen_index.add(values[j]);
+//                        break;
+//                    case 14:
+//                        fourteen_index.add(values[j]);
+//                        break;
+//                    case 15:
+//                        fifteen_index.add(values[j]);
+//                        break;
+//                    case 16:
+//                        sixteen_index.add(values[j]);
+//                        break;
+//                    case 17:
+//                        seventeen_index.add(values[j]);
+//                        break;
+//                    case 18:
+//                        eighteen_index.add(values[j]);
+//                        break;
+//                    case 19:
+//                        nineteen_index.add(values[j]);
+//                        break;
+//                    case 20:
+//                        twenty_index.add(values[j]);
+//                        break;
                 }
             }
         }
-        for (int k = 0; k < 20; k++) {
+        for (int k = 0; k < 3; k++) {
             switch(k) {
                 case 0:
                     indexOfFieldAndValues.put(k, zero_index);
@@ -202,57 +199,57 @@ public class TicketTranslation {
                 case 2:
                     indexOfFieldAndValues.put(k, two_index);
                     break;
-                case 3:
-                    indexOfFieldAndValues.put(k, three_index);
-                    break;
-                case 4:
-                    indexOfFieldAndValues.put(k, four_index);
-                    break;
-                case 5:
-                    indexOfFieldAndValues.put(k, six_index);
-                    break;
-                case 6:
-                    indexOfFieldAndValues.put(k, seven_index);
-                    break;
-                case 7:
-                    indexOfFieldAndValues.put(k, eight_index);
-                    break;
-                case 8:
-                    indexOfFieldAndValues.put(k, nine_index);
-                    break;
-                case 9:
-                    indexOfFieldAndValues.put(k, ten_index);
-                    break;
-                case 10:
-                    indexOfFieldAndValues.put(k, eleven_index);
-                    break;
-                case 11:
-                    indexOfFieldAndValues.put(k, twelve_index);
-                    break;
-                case 12:
-                    indexOfFieldAndValues.put(k, thirteen_index);
-                    break;
-                case 13:
-                    indexOfFieldAndValues.put(k, fourteen_index);
-                    break;
-                case 14:
-                    indexOfFieldAndValues.put(k, fifteen_index);
-                    break;
-                case 15:
-                    indexOfFieldAndValues.put(k, sixteen_index);
-                    break;
-                case 16:
-                    indexOfFieldAndValues.put(k, seventeen_index);
-                    break;
-                case 17:
-                    indexOfFieldAndValues.put(k, eighteen_index);
-                    break;
-                case 18:
-                    indexOfFieldAndValues.put(k, nineteen_index);
-                    break;
-                case 19:
-                    indexOfFieldAndValues.put(k, twenty_index);
-                    break;
+//                case 3:
+//                    indexOfFieldAndValues.put(k, three_index);
+//                    break;
+//                case 4:
+//                    indexOfFieldAndValues.put(k, four_index);
+//                    break;
+//                case 5:
+//                    indexOfFieldAndValues.put(k, six_index);
+//                    break;
+//                case 6:
+//                    indexOfFieldAndValues.put(k, seven_index);
+//                    break;
+//                case 7:
+//                    indexOfFieldAndValues.put(k, eight_index);
+//                    break;
+//                case 8:
+//                    indexOfFieldAndValues.put(k, nine_index);
+//                    break;
+//                case 9:
+//                    indexOfFieldAndValues.put(k, ten_index);
+//                    break;
+//                case 10:
+//                    indexOfFieldAndValues.put(k, eleven_index);
+//                    break;
+//                case 11:
+//                    indexOfFieldAndValues.put(k, twelve_index);
+//                    break;
+//                case 12:
+//                    indexOfFieldAndValues.put(k, thirteen_index);
+//                    break;
+//                case 13:
+//                    indexOfFieldAndValues.put(k, fourteen_index);
+//                    break;
+//                case 14:
+//                    indexOfFieldAndValues.put(k, fifteen_index);
+//                    break;
+//                case 15:
+//                    indexOfFieldAndValues.put(k, sixteen_index);
+//                    break;
+//                case 16:
+//                    indexOfFieldAndValues.put(k, seventeen_index);
+//                    break;
+//                case 17:
+//                    indexOfFieldAndValues.put(k, eighteen_index);
+//                    break;
+//                case 18:
+//                    indexOfFieldAndValues.put(k, nineteen_index);
+//                    break;
+//                case 19:
+//                    indexOfFieldAndValues.put(k, twenty_index);
+//                    break;
             }
         }
     }
@@ -267,6 +264,7 @@ public class TicketTranslation {
                 int value = Integer.parseInt(indexOfFieldAndValues.get(index).get(i));
                 int[] rule = rules.get(k);
                 if(value < rule[0] || value > rule[1] && value < rule[2] || value > rule[3]) {
+                    System.out.println("FOR RULE :" + k + " " + value + " VALUE DOESNT FIT ");
                     ruleBoolean = false;
                     break;
                 }
@@ -276,20 +274,28 @@ public class TicketTranslation {
         return rulesVsIndex;
     }
 
+    // Fields and values can overlap. Values dont exclusively fit in one rule
+
     public void test() {
         getRules();
         getNearbyTickets();
         getErrors();
         populateIndexToValueMap();
-        List<Boolean> listOfIndexVsRules = findField(0);
-        for (int i = 0; i<  listOfIndexVsRules.size(); i++) {
-            System.out.println(listOfIndexVsRules.get(i));
+        List<Integer> fieldsOfInterest = new ArrayList<>();
+        for(int i = 0; i < indexOfFieldAndValues.size(); i++) {
+            List<Boolean> listOfIndexVsRules = findField(i);
+            if(listOfIndexVsRules.get(1) == true){
+//            if(listOfIndexVsRules.get(0) == true || listOfIndexVsRules.get(1) == true || listOfIndexVsRules.get(2) == true || listOfIndexVsRules.get(3) == true || listOfIndexVsRules.get(4) == true || listOfIndexVsRules.get(5) == true || listOfIndexVsRules.get(6) == true ) {
+                fieldsOfInterest.add(i);
+                System.out.println(i);
+            }
         }
+
         System.out.println("FIRST COLUMN : " + indexOfFieldAndValues.get(0));
-        System.out.println("MAP SIZE : " + indexOfFieldAndValues.size());
-        System.out.println("NEARby TICKETS SIZE " + nearbyTickets.size());
-        System.out.println("VALID NEARBY TICKET SIZE " + validNearbyTickets.size());
-        System.out.println(errors.size());
+//        System.out.println("MAP SIZE : " + indexOfFieldAndValues.size());
+//        System.out.println("NEARby TICKETS SIZE " + nearbyTickets.size());
+//        System.out.println("VALID NEARBY TICKET SIZE " + validNearbyTickets.size());
+//        System.out.println(errors.size());
     }
 
 }
