c04078a724d19206d3107f7fc47d89c95e1cbb73
Jamie <jamiewong63@googlemail.com>
2021-02-04 08:57:39 +0000

    day seventeen part one conways cubes read initial grid info

--- src/com/daySeventeen/ConwaysCubes.java
+++ src/com/daySeventeen/ConwaysCubes.java
@@ -1,7 +1,25 @@
 package com.daySeventeen;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ConwaysCubes {
 
+    public void readInitialGrid() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/daySeventeen/initialGrid.txt"
+            ));
+            String line = reader.readLine();
+            line = reader.readLine();
+            reader.close();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
--- src/com/daySeventeen/Cube.java
+++ src/com/daySeventeen/Cube.java
@@ -2,16 +2,14 @@ package com.daySeventeen;
 
 public class Cube {
 
-    int xCor;
-    int yCor;
-    int zCor;
     boolean active;
 
     public Cube(int x, int y, int z, boolean state) {
-        xCor = x;
-        yCor = y;
-        zCor = z;
         active = state;
+    }
+
+    public boolean getState() {
+        return active;
     }
 
 }
