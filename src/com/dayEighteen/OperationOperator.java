23418057bf9877d9622535889930e7a9894e96e0
Jamie <jamiewong63@googlemail.com>
2021-03-19 19:45:14 +0000

    day eighteen read operations data

--- src/com/dayEighteen/OperationOperator.java
+++ src/com/dayEighteen/OperationOperator.java
@@ -1,4 +1,31 @@
 package com.dayEighteen;
 
+import com.daySeventeen.Cube;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class OperationOperator {
+
+    List<String> operations;
+
+    public void readInitialGrid() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/dayEighteen/operations.txt"
+            ));
+            String line = reader.readLine();
+            while (line != null) {
+                operations.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
