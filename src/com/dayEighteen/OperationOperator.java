534a7a79796606e3261e0fe5e0942582a9271e99
Jamie <jamiewong63@googlemail.com>
2021-03-22 23:31:52 +0000

    day eighteen splitting equations into smaller parts

--- src/com/dayEighteen/OperationOperator.java
+++ src/com/dayEighteen/OperationOperator.java
@@ -8,6 +8,7 @@ import javax.script.ScriptException;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 public class OperationOperator {
@@ -43,9 +44,25 @@ public class OperationOperator {
     }
 
     public void test() throws ScriptException {
+        String eq_1 = "2 * 3 + (4 * 5)";
+        String eq_2 = "5 + (8 * 3 + 9 + 3 * 4 * 3)";
         // Should print 26
-        System.out.println(equateOperation("2 * 3 + (4 * 5)"));
+        System.out.println(equateOperation(eq_1));
         // Should print 437
-        System.out.println(equateOperation("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+        System.out.println(sumEquations(splitEquationIntoArray(eq_2)));
+//        System.out.println(Arrays.toString(splitEquationIntoArray(eq_2)));
+    }
+
+    private String[] splitEquationIntoArray(String equation) {
+        String[] splitEquation = equation.split("(?=[0-9]+(?![^(]*\\)))|(?=\\()|(?<=\\))");
+        return splitEquation;
+    }
+
+    private int sumEquations(String[] equationSplitIntoArray) throws ScriptException {
+        int answer = 0;
+        for (int i = 0; i <= equationSplitIntoArray.length; i++) {
+            answer += equateOperation(equationSplitIntoArray[i]);
+        }
+        return answer;
     }
 }
