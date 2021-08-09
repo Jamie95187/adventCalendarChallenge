d776f37352e0828bf29aa407bd14ea48dff6325e
Jamie <jamiewong63@googlemail.com>
2021-08-09 23:56:58 +0100

    day nineteen: solution for 2:a,1:2,0:1

--- src/com/dayNineteen/MonsterMessage.java
+++ src/com/dayNineteen/MonsterMessage.java
@@ -7,7 +7,8 @@ public class MonsterMessage {
     public String solver() {
         HashMap<Integer, String> hmap = new HashMap<Integer, String>();
         hmap.put(0, "1");
-        hmap.put(1, "a");
-        return hmap.get(Integer.parseInt(hmap.get(0)));
+        hmap.put(1, "2");
+        hmap.put(2, "a");
+        return hmap.get(Integer.parseInt(hmap.get(Integer.parseInt(hmap.get(0)))));
     }
 }
