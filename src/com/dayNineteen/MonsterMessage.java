1df386efbb61b08001540984aaf886b4c9c52192
Jamie <jamiewong63@googlemail.com>
2021-08-10 23:43:21 +0100

    day nineteen: return a when message is 0:1,1:3,2:4,3:a,4:b

--- src/com/dayNineteen/MonsterMessage.java
+++ src/com/dayNineteen/MonsterMessage.java
@@ -7,8 +7,11 @@ public class MonsterMessage {
     public String solver() {
         HashMap<Integer, String> hmap = new HashMap<Integer, String>();
         hmap.put(0, "1");
-        hmap.put(1, "2");
-        hmap.put(2, "a");
+        hmap.put(1, "3");
+        hmap.put(2, "4");
+        hmap.put(3, "a");
+        hmap.put(4, "b");
         return hmap.get(Integer.parseInt(hmap.get(Integer.parseInt(hmap.get(0)))));
     }
+
 }
