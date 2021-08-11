0e803ca0a487e080e85ed4d6bf89c4507152b2c9
Jamie <jamiewong63@googlemail.com>
2021-08-11 03:40:40 +0100

    day nineteen: add message data

--- src/com/dayNineteen/MonsterMessage.java
+++ src/com/dayNineteen/MonsterMessage.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class MonsterMessage {
 
     public String solver() {
-        HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+        HashMap<Integer, String> hmap = new HashMap<>();
         hmap.put(0, "1");
         hmap.put(1, "3");
         hmap.put(2, "4");
