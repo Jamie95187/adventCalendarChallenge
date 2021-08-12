d087ab4b28e27766f36854eaea71304a3dce828c
Jamie <jamiewong63@googlemail.com>
2021-08-12 22:30:26 +0100

    day nineteen add file reader

--- src/com/dayNineteen/MonsterMessage.java
+++ src/com/dayNineteen/MonsterMessage.java
@@ -1,8 +1,29 @@
 package com.dayNineteen;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class MonsterMessage {
+
+    public void readMessages() {
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/dayNineteen/messageData.txt"
+            ));
+            String line = reader.readLine();
+            while (line != null) {
+                line = reader.readLine();
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public String solver() {
         HashMap<Integer, String> hmap = new HashMap<>();
