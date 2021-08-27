cf263b35e0750ddb291ae0877a7d9b019f0abff1
Jamie <jamiewong63@googlemail.com>
2021-08-27 23:50:00 +0100

    day nineteen: continue solving for one iteration of zero message

--- src/com/dayNineteen/MonsterMessage.java
+++ src/com/dayNineteen/MonsterMessage.java
@@ -11,7 +11,7 @@ public class MonsterMessage {
     private ArrayList<Rule> messages = new ArrayList<> (130);
     private ArrayList<String> correctRules = new ArrayList<> ();
     private int numberOfCorrectRules = 0;
-    private String zeroMessage;
+    private String zeroMessage = " ";
 
     public void readMessages() {
         for (int i = 0; i < 130; i++) {
@@ -26,7 +26,9 @@ public class MonsterMessage {
             int counter = 1;
             while (line != null && counter < 130) {
                 int index = Integer.parseInt(line.substring(0, line.indexOf(':')));
-                System.out.println(line);
+                if (index == 0) {
+                    zeroMessage = line.substring(line.indexOf(':')+1);
+                }
                 if (line.contains("|")) {
                     messages.get(index).setLeftRule(line.substring(line.indexOf(':')+1, line.indexOf("|")));
                     messages.get(index).setRightRule(line.substring(line.indexOf("|")).trim());
@@ -43,26 +45,8 @@ public class MonsterMessage {
         }
     }
 
-    public int solver() {
-        HashMap<Integer, String> hmap = new HashMap<>();
-        hmap.put(0, "1 2");
-        hmap.put(1, "a");
-        hmap.put(2, "1 3 | 3 1");
-        hmap.put(3, "b");
-        int counter = 0;
-        int firstMessageIndex = Integer.parseInt(hmap.get(0).split(" ")[0]);
-        int secondMessageIndex = Integer.parseInt(hmap.get(0).split(" ")[1]);
-        if (hmap.get(firstMessageIndex) == "a") {
-            counter++;
-        }
-        String message = hmap.get(secondMessageIndex);
-        String firstHalfMessage = message.split("\\|")[0];
-        String secondHalfMessage = message.split("\\|")[1];
-        if ( hmap.get(Integer.parseInt(firstHalfMessage.split(" ")[0])) == "a" && hmap.get(Integer.parseInt(firstHalfMessage.split(" ")[1])) == "b"
-                || hmap.get(Integer.parseInt(secondHalfMessage.split(" ")[0])) == "b" && hmap.get(Integer.parseInt(secondHalfMessage.split(" ")[1])) == "a") {
-            counter++;
-        }
-        return counter;
+    public void solverFOrOne() {
+        String[] zeroMessageSplit = zeroMessage.split(" ");
     }
 
     public void iterator() {
