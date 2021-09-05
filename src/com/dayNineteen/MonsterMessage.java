ec48bc3eb9df3f2a673226076a417342c590aa3a
Jamie <jamiewong63@googlemail.com>
2021-09-05 18:09:38 +0100

    day nineteen: solve for right rule

--- src/com/dayNineteen/MonsterMessage.java
+++ src/com/dayNineteen/MonsterMessage.java
@@ -60,7 +60,6 @@ public class MonsterMessage {
                 continue;
             }
             String leftRule = messages.get(Integer.parseInt(s)).left.trim();
-//            System.out.println(leftRule);
             if (leftRule.contains(" ")) {
                 if (leftRule.split(" ")[0].equals("64") | leftRule.split(" ")[0].equals("50")) {
                     list.addFirst(leftRule.split(" ")[1]);
@@ -82,6 +81,53 @@ public class MonsterMessage {
         }
         for (String index : solvedList) {
 //            System.out.println(index);
+            if (index.equals("64")) {
+                message = message + "a";
+            } else {
+                message = message + "b";
+            }
+        }
+        correctMessages.add(message);
+    }
+
+    public void solverForRightHandSide() {
+        LinkedList<String> list = new LinkedList<String>();
+        LinkedList<String> solvedList = new LinkedList<String>();
+        String message = "";
+        for (String s : zeroMessage) {
+            list.add(s);
+        }
+        while (!list.isEmpty()) {
+            String s = list.removeFirst();
+            if (s.equals("64") | s.equals("50")) {
+                solvedList.add(s);
+                continue;
+            }
+            String rule = "";
+            if (messages.get(Integer.parseInt(s)).right.equals("")){
+                rule = messages.get(Integer.parseInt(s)).left.trim();
+            } else {
+                rule = messages.get(Integer.parseInt(s)).right.trim();
+            }
+            if (rule.contains(" ")) {
+                if (rule.split(" ")[0].equals("64") | rule.split(" ")[0].equals("50")) {
+                    list.addFirst(rule.split(" ")[1]);
+                    solvedList.add(rule.split(" ")[0]);
+                } else if (rule.split(" ")[0].equals("64") | rule.split(" ")[0].equals("50")
+                        && rule.split(" ")[1].equals("64") | rule.split(" ")[1].equals("50")) {
+                    solvedList.add(rule.split(" ")[0]);
+                    solvedList.add(rule.split(" ")[1]);
+                } else {
+                    list.addFirst(rule.split(" ")[1]);
+                    list.addFirst(rule.split(" ")[0]);
+                }
+            } else {
+                if (!rule.equals("64") | !rule.equals("50")) {
+                    list.addFirst(rule);
+                }
+            }
+        }
+        for (String index : solvedList) {
             if (index.equals("64")) {
                 message = message + "a";
             } else {
