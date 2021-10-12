123a608584b447c79906ed1b210f5fcf7a8baa51
Jamie <jamiewong63@googlemail.com>
2021-10-12 17:08:00 +0100

    day nineteen: example data two

--- src/com/dayNineteen/MonsterMessage.java
+++ src/com/dayNineteen/MonsterMessage.java
@@ -11,68 +11,34 @@ public class MonsterMessage {
 
     private ArrayList<Rule> messages = new ArrayList<> (130);
 //    private ArrayList<Rule> exampleData = new ArrayList<> (6);
+    private ArrayList<Rule> exampleDataTwo = new ArrayList<> (4);
     public Queue<String> queue = new LinkedList<>();
     public ArrayList<String> validRules = new ArrayList<>();
     public ArrayList<String> messagesToBeChecked = new ArrayList<>();
 
-    public void readMessages() {
-        for (int i = 0; i < 130; i++) {
-            messages.add(new Rule());
-        }
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(
-                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/dayNineteen/messageData.txt"
-            ));
-            String line = reader.readLine();
-            int counter = 1;
-            while (line != null && counter < 130) {
-                int index = Integer.parseInt(line.substring(0, line.indexOf(':')));
-                if (line.contains("|")) {
-                    messages.get(index).setLeftRule(line.substring(line.indexOf(':')+1, line.indexOf("|")));
-                    messages.get(index).setRightRule(line.substring(line.indexOf("|")+1).trim());
-                    messages.get(index).hasRight = true;
-                } else {
-                    messages.get(index).setLeftRule(line.substring(line.indexOf(':')+1));
-                }
-                counter++;
-                line = reader.readLine();
-            }
-            line = reader.readLine();
-            while (line != null) {
-                messagesToBeChecked.add(line);
-                line = reader.readLine();
-            }
-            reader.close();
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-//    public void readExampleData() {
-//        for (int i = 0; i < 5; i++) {
-//            exampleData.add(new Rule());
+//    public void readMessages() {
+//        for (int i = 0; i < 130; i++) {
+//            messages.add(new Rule());
 //        }
 //        BufferedReader reader;
 //        try {
 //            reader = new BufferedReader(new FileReader(
-//                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/dayNineteen/exampleData.txt"
+//                    "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/dayNineteen/messageData.txt"
 //            ));
 //            String line = reader.readLine();
-//            int counter = 0;
-//            while (line != null && counter < 5) {
+//            int counter = 1;
+//            while (line != null && counter < 130) {
 //                int index = Integer.parseInt(line.substring(0, line.indexOf(':')));
 //                if (line.contains("|")) {
-//                    exampleData.get(index).setLeftRule(line.substring(line.indexOf(':')+1, line.indexOf("|")));
-//                    exampleData.get(index).setRightRule(line.substring(line.indexOf("|")+1).trim());
+//                    messages.get(index).setLeftRule(line.substring(line.indexOf(':')+1, line.indexOf("|")));
+//                    messages.get(index).setRightRule(line.substring(line.indexOf("|")+1).trim());
+//                    messages.get(index).hasRight = true;
 //                } else {
-//                    exampleData.get(index).setLeftRule(line.substring(line.indexOf(':')+1));
+//                    messages.get(index).setLeftRule(line.substring(line.indexOf(':')+1));
 //                }
 //                counter++;
 //                line = reader.readLine();
 //            }
-//            reader.readLine();
 //            line = reader.readLine();
 //            while (line != null) {
 //                messagesToBeChecked.add(line);
@@ -85,28 +51,76 @@ public class MonsterMessage {
 //        }
 //    }
 
+    public void readExampleData() {
+//        for (int i = 0; i < 5; i++) {
+//            exampleData.add(new Rule());
+//        }
+        for (int i = 0; i < 4; i++) {
+            exampleDataTwo.add(new Rule());
+        }
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(
+        "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/dayNineteen/exampleDataTwo.txt"
+//               "/Users/jamie/IdeaProjects/AdventCalendarPuzzles/out/production/AdventCalendarPuzzles/com/dayNineteen/exampleData.txt"
+            ));
+            String line = reader.readLine();
+            int counter = 0;
+            while (line != null && counter < 4) {
+                int index = Integer.parseInt(line.substring(0, line.indexOf(':')));
+                if (line.contains("|")) {
+                    exampleDataTwo.get(index).setLeftRule(line.substring(line.indexOf(':')+1, line.indexOf("|")));
+                    exampleDataTwo.get(index).setRightRule(line.substring(line.indexOf("|")+1).trim());
+//                    exampleData.get(index).setLeftRule(line.substring(line.indexOf(':')+1, line.indexOf("|")));
+//                    exampleData.get(index).setRightRule(line.substring(line.indexOf("|")+1).trim());
+                } else {
+                    exampleDataTwo.get(index).setLeftRule(line.substring(line.indexOf(':')+1));
+//                    exampleData.get(index).setLeftRule(line.substring(line.indexOf(':')+1));
+                }
+                counter++;
+                line = reader.readLine();
+            }
+            reader.readLine();
+            line = reader.readLine();
+            while (line != null) {
+                messagesToBeChecked.add(line);
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void addToInitialStack() {
-        readMessages();
+//        readMessages();
 //        Used for testing example data
-//        readExampleData();
+        readExampleData();
+        queue.add(exampleDataTwo.get(0).left.trim());
 //        queue.add(exampleData.get(0).left.trim());
-        queue.add(messages.get(0).left.trim());
+//        queue.add(messages.get(0).left.trim());
     }
 
     public void checkRule(String rule) {
         String[] splitRule = rule.trim().split(" ");
 //        Used for testing example data
 //        ArrayList<Rule> messagesExample = exampleData;
-        ArrayList<Rule> messagesExample = messages;
+        ArrayList<Rule> messagesExample = exampleDataTwo;
+//        ArrayList<Rule> messagesExample = messages;
         String left = "";
         String right= "";
         int index = 0;
         for (int i = 0; i < splitRule.length; i++) {
             // Not equal to rule where they return "a" or "b"
-            if (!splitRule[i].equals("50") && !splitRule[i].equals("64")) {
+            if (!splitRule[i].equals("1") && !splitRule[i].equals("3")) {
                 index = i;
                 break;
             }
+//            if (!splitRule[i].equals("50") && !splitRule[i].equals("64")) {
+//                index = i;
+//                break;
+//            }
         }
 
         for (int i = 0; i < splitRule.length; i++) {
@@ -133,9 +147,12 @@ public class MonsterMessage {
 
     public boolean testCheck(String message){
         for (String s : message.split(" ")) {
-            if (!s.trim().equals("50") & !s.trim().equals("64")) {
+            if (!s.trim().equals("1") & !s.trim().equals("3")) {
                 return false;
             }
+//            if (!s.trim().equals("50") & !s.trim().equals("64")) {
+//                return false;
+//            }
         }
         return true;
     }
@@ -155,7 +172,7 @@ public class MonsterMessage {
     
     public void testPrintQueue() {
         for (String s : queue) {
-            System.out.printwln(s);
+            System.out.println(s);
         }
     }
 
@@ -163,12 +180,18 @@ public class MonsterMessage {
         for (String rule : rules){
             String validRule = "";
             for(String index: rule.split(" ")) {
-                if (index.equals("64")) {
+                if (index.equals("1")) {
                     validRule += "a";
                 }
-                if (index.equals("50")) {
+                if (index.equals("3")) {
                     validRule += "b";
                 }
+//                if (index.equals("64")) {
+//                    validRule += "a";
+//                }
+//                if (index.equals("50")) {
+//                    validRule += "b";
+//                }
             }
             validRules.add(validRule);
         }
